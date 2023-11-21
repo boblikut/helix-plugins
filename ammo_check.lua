0c70184737514f004d969cb3b059bc6fc2c1da27
boblikut <125297488+2007MICHAIL2007@users.noreply.github.com>
2023-11-21 09:02:18 +0300

    Update ammo_check.lua

--- ammo_check.lua
+++ ammo_check.lua
@@ -26,38 +26,24 @@ local function setDelay(delay)
     lastUse = CurTime() + delay
 end 
 
-BanWeapons = {'ix_keys', 'ix_hands'} -- melee weapon and other weapons withot clips
-
-local function IsBanWeapon(bw, ply) 
-
-local ShouldBanWeapon = false
-
-for k, v in ipairs(bw) do
-if v == ply:GetActiveWeapon():GetClass() then
-ShouldBanWeapon = true
-end
-end
-
-return ShouldBanWeapon
-
-end
+local BanWeapons = {
+ix_keys = true,
+ix_hands = true
+ } -- melee weapon and other weapons withot clips
 
 
 -------------------------------------------------------------------------------------------------------------------- Begining of general hook
 hook.Add("PlayerButtonDown", "PressCheckButtun", function(ply, button) 
-pcall(function()
-if !IsBanWeapon(BanWeapons,ply) and checkDelay() and button == KEY_O then
+
+if !BanWeapons[ply:GetActiveWeapon():GetClass()] and checkDelay() and button == KEY_O then
+
 setDelay(3)
 ply.ShouldBreak = false
-
-hook.Add( "PlayerSwitchWeapon", "StopSwitchWeapon", function( ply, oldWeapon, newWeapon ) -- Player can't swintch weapon
-	return true
-end )
-
+ply.ShouldStopSwitchWeapon = true
 
 ply:SetFOV(85,2)
 ply:EmitSound("weapons/ar2/npc_ar2_reload.wav", 75, 90, 0.5)
-ply:Say('/me проверяет кол - во патронов в магазине')
+pcall(function() ply:Say('/me проверяет кол - во патронов в магазине') 
 
 ply:SetAction('ПРОВЕРЯЮ КОЛ - ВО ПАТРОНОВ В МАГАЗИНЕ...', 2, function()
 
@@ -65,25 +51,23 @@ if not ply.ShouldBreak then
 ply:SetFOV(0,0.5)
 ply:ChatPrint(string.format('В магазине %d/%d патронов', ply:GetActiveWeapon():Clip1(), ply:GetActiveWeapon():GetMaxClip1()))
 ply:Say('/me закончил проверку кол - ва патронов в магазине')
-hook.Remove( "PlayerSwitchWeapon", "StopSwitchWeapon" )
+ply.ShouldStopSwitchWeapon = false
 end
 
 end)
-
+end)
 
 end
 
-if !IsBanWeapon(BanWeapons,ply) and checkDelay() and button == KEY_I then
+if !BanWeapons[ply:GetActiveWeapon():GetClass()] and checkDelay() and button == KEY_I then
 setDelay(3)
 ply.ShouldBreak = false
-
-hook.Add( "PlayerSwitchWeapon", "StopSwitchWeapon", function( ply, oldWeapon, newWeapon ) -- Player can't swintch weapon
-	return true
-end )
+ply.ShouldStopSwitchWeapon = true
 
 
 ply:SetFOV(85,2)
 ply:EmitSound("weapons/ar2/npc_ar2_reload.wav", 75, 90, 0.5)
+pcall(function()
 ply:Say('/me проверяет кол - во патрон с собой')
 
 ply:SetAction('ПРОВЕРЯЮ КОЛ - ВО ПАТРОН В РЮКЗАКЕ...', 2, function()
@@ -92,16 +76,16 @@ if not ply.ShouldBreak then
 ply:SetFOV(0,0.5)
 ply:ChatPrint(string.format('Вы сможете полностью зарядить оружие ещё %d раз(а)', math.floor(ply:GetAmmoCount(ply:GetActiveWeapon():GetPrimaryAmmoType())/ply:GetActiveWeapon():GetMaxClip1())))
 ply:Say('/me закончил проверку кол - ва патронов с собой')
-hook.Remove( "PlayerSwitchWeapon", "StopSwitchWeapon" )
+ply.ShouldStopSwitchWeapon = false
 end
 
 end)
-
+end)
 
 end
 
 
-end)
+
 end)
 -------------------------------------------------------------------------------------------------------------------- Ending of general hook
 
@@ -113,8 +97,12 @@ hook.Add( "EntityFireBullets", "Stop Checking", function( entity, data ) -- Shoo
 	ply:SetFOV(0,0.01)
 	hook.Remove( "PlayerSwitchWeapon", "StopSwitchWeapon" )
 	pcall(function() ply:SetAction() end)
-
-
-
 	
+end )
+
+
+hook.Add( "PlayerSwitchWeapon", "StopSwitchWeapon", function( ply, oldWeapon, newWeapon ) -- Player can't swintch weapon
+if ply.ShouldStopSwitchWeapon then
+	return true
+	end
 end )
