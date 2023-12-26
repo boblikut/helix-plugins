46cea464362154dc5da682813c8f5f57dba87489
boblikut <125297488+2007MICHAIL2007@users.noreply.github.com>
2023-12-26 19:43:22 +0300

    Create tfa_hud_off.lua

---
+++ tfa_hud_off.lua
@@ -0,0 +1,13 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "TFA HUD offer"
+PLUGIN.description = "Off tfa dynamic HUD and TFA crosshair"
+PLUGIN.schema = "Any"
+
+function PLUGIN:PlayerInitialSpawn(ply, trans)
+  ply:SetLocalVar("IsTFAHUDHided", false)
+  ply:SendLua(string.format([[RunConsoleCommand("cl_tfa_hud_crosshair_enable_custom",0)]]))
+  ply:SendLua(string.format([[RunConsoleCommand("cl_tfa_hud_enabled",0)]]))
+  ply:SendLua(string.format([[RunConsoleCommand("cl_tfa_hud_fallback_enabled",0)]]))
+  ply:SetLocalVar("IsTFAHUDHided", true)
+end
