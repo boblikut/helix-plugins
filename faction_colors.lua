133a915171401d173a7c99ef231e0a0c5360dd31
boblikut <125297488+boblikut@users.noreply.github.com>
2024-07-12 16:36:10 +0300

    Create faction_colors.lua

---
+++ faction_colors.lua
@@ -0,0 +1,16 @@
+local PLUGIN = PLUGIN
+
+PLUGIN.name = "Faction colors"
+PLUGIN.author = "boblikut"
+PLUGIN.description = "Automaticaly set schema color for all factions"
+
+function SetFactionsColor()
+	for _, FACTION in pairs(ix.faction.indices) do
+		FACTION.color = ix.config.Get("color", Color(150, 125, 100, 255))
+		team.SetUp(FACTION.index, FACTION.name or "Unknown", FACTION.color or Color(125, 125, 125))
+	end
+end
+
+function PLUGIN:InitializedConfig()
+	SetFactionsColor()
+end
